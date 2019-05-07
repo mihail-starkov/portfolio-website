488a83b228eb3b25ecc610f960af683329324142
Mihail Starkov <mikestarkb@gmail.com>
2019-05-07 09:35:42 +0300

    Fix styles for image Photo- add mix-width and min-height

--- dev/src/modules/SwipeMenu/SwipeMenu.js
+++ dev/src/modules/SwipeMenu/SwipeMenu.js
@@ -1,10 +1,11 @@
 'use strict';
 
-let SwipeMenu = document.querySelector('.Swipe-Menu');
-SwipeMenu.addEventListener('click', (e) => {
-  if (e.target.classList.contains('Menu-link')) {
-    document.querySelector('.Swipe').classList.remove('Swipe_active');
-    document.querySelector('.ButtonBurger').classList.remove('ButtonBurger_active');
-    document.querySelector('.Content').classList.remove('Content_active');
-  }
-});
+document.querySelector('.Swipe-Menu')
+  .addEventListener('click', (e) => {
+    if (e.target.classList.contains('Menu-link')) {
+      document.querySelector('.Swipe').classList.remove('Swipe_active');
+      document.querySelector('.ButtonBurger').classList.remove('ButtonBurger_active');
+      document.querySelector('.Content').classList.remove('Content_active');
+      
+    }
+  });
