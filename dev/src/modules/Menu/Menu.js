07a5abd182c82be9361bc130bce8b1f5adf69a7c
Mihail Starkov <mikestarkb@gmail.com>
2019-05-15 09:35:03 +0300

    Rewrite and create object menu

--- dev/src/modules/Menu/Menu.js
+++ dev/src/modules/Menu/Menu.js
@@ -2,59 +2,80 @@
 
 /**
  * Объект меню
- * @property {CSS} defaultMenuClass Класс контейнера меню для метода render
- * @property {CSS} defaultMenuListClass Класс ul для метода render
- * @property {CSS} defaultMenuListItemClass Класс li для метода render
- * @property {CSS} headerMenuClass Класс контейнера меню Header
- * @property {CSS} headerMenuListClass Класс для элемента Header ul
- * @property {CSS} headerMenuItemClass Класс для элемента Header li
- * @property {CSS} footerMenuClass Класс контейнера меню Footer
- * @property {CSS} footerMenuListClass Класс для элемента Footer ul
- * @property {CSS} footerMenuItemClass Класс для элемента Footer li
- * @property {CSS} infoMenuClass Класс контейнера меню info
- * @property {CSS} infoMenuListClass Класс для элемента info ul
- * @property {CSS} InfoMenuItemClass Класс для элемента info li
- * @property {CSS} burgerMenuClass Класс контейнера меню burger
- * @property {CSS} burgerMenuListClass Класс для элемента burger ul
- * @property {CSS} burgerMenuListItemClass Класс для элемента burger li
- * @property {CSS} subMenuListClass Класс контейнера меню submenu ul
- * @property {CSS} subMenuItemClass Класс для элемента submenu li
- * @property {CSS} btnBurgerId Класс для кнопки burgerMenu
  */
 const menu = {
-  init() {
-    let menu = document.querySelectorAll('.Menu');
-    
-    for (let i = 0; i < menu.length; i++) {
-      menu[i].addEventListener('click', (e) => {
-        this.removeStyleClass();
-        if ((e.target.classList.contains('Menu-link'))) {
-          this.setStyleClass(e.target);
-        }
-      });
-    }
+  /**
+   * DTO Объект с настройками по умолчанию
+   * @property {string} menuClass CSS Class для всех меню на странице
+   * @property {string} menuLinkClass CSS Class для всех ссылок на странице
+   * @property {string} menuLinkModActiveClass CSS Class для активированный ссылки(по которой перешли)
+   * @property {string} dataAttr DATA аттрибут, содержащий CSS Id секции, к которой необходимо перейти
+   */
+  settings: {
+    menuClass: 'Menu',
+    menuLinkClass: 'Menu-link',
+    menuLinkModActiveClass: 'Menu-link_active',
+    dataAttr: 'data-section',
   },
   /**
-   * Метод удаляет класс Css у элемента
+   *Метод инициализации скрипта
+   */
+  init(userSettings = {}) {
+    this.settings = Object.assign(this.settings, userSettings);
+    document
+      .querySelectorAll(`.${this.settings.menuClass}`)
+      .forEach(menu => {
+        menu.addEventListener('click', (e) => {
+          e.preventDefault();
+          if ((e.target.classList.contains(`${this.settings.menuLinkClass}`))) {
+            this.moveToSection(e.target);
+            this.removeStyleClass();
+            this.setStyleClass(e.target);
+          }
+        });
+      });
+    
+  },
+  /**
+   * Метод прокручивает страницу к нужной секции
+   * @param {EventTarget} link элемент на котором произошло событие клик
+   */
+  moveToSection(link) {
+    document
+      .getElementById(this.getDesiredSection(link))
+      .scrollIntoView({behavior: 'smooth', block: 'start'});
+  },
+  /**
+   * Метод Получает нужную секцию, к которой нужно перейти и отдает
+   * @param {EventTarget} link элемент на котором произошло событие клик
+   * @return {string} Возвращает CSS Id из аттрибута для перехода к нужной секции страницы
+   */
+  getDesiredSection(link) {
+    return link.getAttribute(this.settings.dataAttr);
+  },
+  /**
+   * Метод удаляет CSS класс у элемента
    */
   removeStyleClass() {
-    let links = document.querySelectorAll('.Menu-link');
-    for (let i = 0; i < links.length; i++) {
-      links[i].classList.remove('Menu-link_active');
-    }
+    document
+      .querySelectorAll(`.${this.settings.menuLinkClass}`)
+      .forEach(link => {
+        link.classList.remove(this.settings.menuLinkModActiveClass);
+      });
   },
   /**
    * Метод устанавливает класс Css у элемента в верхнем и нижнем менню
-   * @param link {HTMLElement} Элемент по которому был произведен клик
+   * @param {EventTarget} transLink Элемент по которому был произведен клик
    */
-  setStyleClass(link) {
-    let links = document.querySelectorAll('.Menu-link');
-    for (let i = 0; i < links.length; i++) {
-      if (link.text === links[i].text) {
-        links[i].classList.add('Menu-link_active');
-      }
-    }
-    link.classList.add('Menu-link_active');
+  setStyleClass(transLink) {
+    document
+      .querySelectorAll(`.${this.settings.menuLinkClass}`)
+      .forEach(link => {
+        if (transLink.text === link.text) {
+          link.classList.add(this.settings.menuLinkModActiveClass);
+        }
+      });
+    transLink.classList.add(this.settings.menuLinkModActiveClass);
   },
 };
 menu.init();
